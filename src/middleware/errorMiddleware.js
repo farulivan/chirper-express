611a9efc0d99faf9687d79fecc756bcccbf2dbe0
farulivan <farulivan@gmail.com>
2024-04-12 23:52:13 +0700

    chore: add jsdoc to error middleware

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -3,7 +3,17 @@ import {
   ErrInternalServer,
 } from '../service/errors/index.js';
 
-function errorMiddleware(error, req, res, next) {
+/**
+ * Express middleware for handling errors throughout the application.
+ * It checks the type of error and responds with appropriate HTTP status codes and messages.
+ * This middleware helps to centralize error handling and make the responses consistent.
+ *
+ * @param {Error} error - The error object thrown by previous middleware or route handlers.
+ * @param {import('express').Request} req - The Express request object.
+ * @param {import('express').Response} res - The Express response object used to send responses to the client.
+ * @param {import('express').NextFunction} next - The next middleware function in the stack.
+ */
+export default function errorMiddleware(error, req, res, next) {
   // Specific handling for ErrForbiddenAccess
   if (error instanceof ErrForbiddenAccess) {
     return res.status(error.statusCode).json({
@@ -29,5 +39,3 @@ function errorMiddleware(error, req, res, next) {
     msg: internalError.message,
   });
 }
-
-export default errorMiddleware;
